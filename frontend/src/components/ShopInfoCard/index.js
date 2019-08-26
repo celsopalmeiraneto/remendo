8b5229eec138f5ddc3b916f05b6872c8aa85f05a
celsopalmeiraneto <celsopalmeiraneto@gmail.com>
2019-08-25 21:51:38 -0300

    Improving the UI of the Point of Interest card.

--- frontend/src/components/ShopInfoCard/index.js
+++ frontend/src/components/ShopInfoCard/index.js
@@ -14,20 +14,24 @@ export class ShopInfoCard extends Component {
     const shop = this.props.shop;
     return (
       <div className={styles.shopInfoCard}>
-        <div className={styles.close}>
-          <button
-            aria-label="close"
-            onClick={() => this.closeComponent()}>
-            <img alt="" src={closeIcon} />
-          </button>
-        </div>
-        <div className={styles.description}>
+        <div className={styles.headerLine}>
           <h1>{shop.name}</h1>
-          <div>Endereço: <span>{shop.address}</span></div>
-          <div>Horário: <span>{shop.operationHours}</span></div>
+          <div className={styles.close}>
+            <button
+              aria-label="close"
+              onClick={() => this.closeComponent()}>
+              <img alt="" src={closeIcon} />
+            </button>
+          </div>
         </div>
-        <div className={styles.photo}>
-          <img src={shop.pictureUrl} alt="Bicicletaria ou Borracharia" />
+        <div className={styles.infoBox}>
+          <div className={styles.description}>
+            <div>Endereço: <span>{shop.address}</span></div>
+            <div>Horário: <span>{shop.operationHours}</span></div>
+          </div>
+          <div className={styles.photo}>
+            <img src={shop.pictureUrl} alt="Bicicletaria ou Borracharia" />
+          </div>
         </div>
       </div>
     );
